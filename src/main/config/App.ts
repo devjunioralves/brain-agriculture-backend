2fc70a7eccf3b8307cc5c2e7585e38c694913cfe
Junior Alves <jrwanderley17@gmail.com>
2023-05-03 20:11:11 -0300

    feat: setup middlewares, routes and app start

--- src/main/config/App.ts
+++ src/main/config/App.ts
@@ -1,4 +1,8 @@
 import express from 'express'
+import setupRoutes from './Routes'
+import setupMiddlewares from './Middlewares'
 
 const app = express()
+setupMiddlewares(app)
+setupRoutes(app)
 export default app
